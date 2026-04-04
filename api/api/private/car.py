e2562fbe4a61d85001d40289f5005000c79d5378
Ihor Savenko <csigorek@gmail.com>
2026-04-04 22:40:07 +0100

    Fix car model

--- api/api/private/car.py
+++ api/api/private/car.py
@@ -66,7 +66,7 @@ async def create_car(
         odometer_unit=car_data.odometer_unit,
         fuel_unit=car_data.fuel_unit,
         current_odometer=car_data.initial_odometer,
-        sales_meta= **car_data.sales_meta.model_dump() if car_data.sales_meta else None,
+        sales_meta= car_data.sales_meta.model_dump() if car_data.sales_meta else None,
         color=car_data.color,
         vin=car_data.vin,
     )
